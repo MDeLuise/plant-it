ce022b7a6ecd48208fdd953db7590de707bf6532
MDeLuise <66636702+MDeLuise@users.noreply.github.com>
2023-06-07 10:29:47 +0200

    Add skeleton to my plant section

--- frontend/src/components/UserPlant.tsx
+++ frontend/src/components/UserPlant.tsx
@@ -1,14 +1,16 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Skeleton, Typography } from "@mui/material";
 import { trackedEntity } from "../interfaces";
 import { Buffer } from "buffer";
 import { isBigScreen } from "../common";
 import { NavigateFunction, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function UserPlant(props: { entity: trackedEntity }) {
     const name: string = props.entity.personalName != undefined ?
         props.entity.personalName :
         "plant " + props.entity.id
     let navigate: NavigateFunction = useNavigate();
+    const [imageLoaded, setImageLoaded] = useState<boolean>(false);
 
     return (
         <Box
@@ -23,17 +25,22 @@ export default function UserPlant(props: { entity: trackedEntity }) {
                 padding: "10px",
                 flexShrink: 0
             }}>
+            {!imageLoaded &&
+                <Skeleton variant="rounded" animation="wave" sx={{ width: "100%", height: "100%" }} />
+            }
             <img
                 src={props.entity.botanicalName.imageUrl}
+                onLoad={() => setImageLoaded(true)}
                 style={{
                     width: "100%",
                     height: "70%",
                     objectFit: "cover",
                     borderRadius: "5px",
                     marginBottom: "10px",
+                    visibility: imageLoaded ? "initial" : "hidden"
                 }}
             />
-            <Typography variant="body1" style={{fontWeight: 600}}>
+            <Typography variant="body1" style={{ fontWeight: 600 }}>
                 {name}
             </Typography>
             <Typography variant="body1">
