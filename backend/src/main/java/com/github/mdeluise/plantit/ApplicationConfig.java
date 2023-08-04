f62635e4e8bc6f7c7257ebf970fe6ff2bf952e1d
MDeLuise <66636702+MDeLuise@users.noreply.github.com>
2023-08-04 10:39:44 +0200

    docs: Add different .ico files
    - Add custom backend endpoint in swagger UI
    - Cleanup of unused files

--- backend/src/main/java/com/github/mdeluise/plantit/ApplicationConfig.java
+++ backend/src/main/java/com/github/mdeluise/plantit/ApplicationConfig.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.ServerVariable;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -31,9 +32,17 @@ import redis.embedded.RedisServer;
         license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0"),
         contact = @Contact(name = "GitHub page", url = "https://github.com/MDeLuise/plant-it")
     ), security = {@SecurityRequirement(name = "bearerAuth")}, servers = {
-    @Server(description = "Developer", url = "http://localhost:8085/api"),
-    @Server(description = "Production", url = "http://localhost:8080/api")
-}
+    @Server(description = "Production", url = "/api"),
+    @Server(
+        description = "Custom",
+        url = "{protocol}://{host}:{port}/{basePath}",
+        variables = {
+            @ServerVariable(name = "protocol", defaultValue = "http", allowableValues = {"http", "https"}),
+            @ServerVariable(name = "host", defaultValue = "localhost"),
+            @ServerVariable(name = "port", defaultValue = "8085"),
+            @ServerVariable(name = "basePath", defaultValue = "api")
+        })
+    }
 )
 @SecurityScheme(
     name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer",
