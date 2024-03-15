f15abae391c94e45ce33af15a6d3cf8657d271ce
MDeLuise <66636702+MDeLuise@users.noreply.github.com>
2024-03-15 08:05:51 +0100

    improve: add parallax effect on plants carousel

---
+++ frontend/lib/plant_list.dart
@@ -0,0 +1,160 @@
+import 'dart:convert';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:plant_it/app_http_client.dart';
+import 'package:plant_it/dto/plant_dto.dart';
+import 'package:plant_it/environment.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class PlantList extends StatefulWidget {
+  final Environment env;
+  const PlantList({super.key, required this.env});
+
+  @override
+  State<StatefulWidget> createState() => _PlantList();
+}
+
+class _PlantList extends State<PlantList> {
+  final controller = PageController(viewportFraction: .8, keepPage: true);
+  double page = 0.0;
+
+  @override
+  void initState() {
+    controller.addListener(() {
+      setState(() {
+        page = controller.page!;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          const SizedBox(height: 16),
+          SizedBox(
+            height: screenSize.width *
+                .8, // height: screenSize.width * .8 // screenSize.height * .55
+            child: PageView.builder(
+              itemCount: widget.env.plants?.length ?? 0,
+              // controller: PageController(
+              //   viewportFraction: isSmallScreen(context) ? .8 : .3,
+              // ),
+              controller: controller,
+              itemBuilder: (_, index) {
+                return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ParallaxPlantCard(
+                        horizontalSlide: (index - page).clamp(-1, 1).toDouble(),
+                        plant: widget.env.plants![index],
+                        http: widget.env.http));
+              },
+            ),
+          ),
+          SmoothPageIndicator(
+            controller: controller,
+            count: widget.env.plants?.length ?? 0,
+            effect: const ScrollingDotsEffect(
+              dotWidth: 5.0,
+              dotHeight: 5.0,
+              activeDotScale: 2,
+              activeDotColor: Color(
+                0xFF6DD075,
+              ),
+            ),
+            onDotClicked: (index) => controller.animateToPage(
+              index,
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.ease,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ParallaxPlantCard extends StatefulWidget {
+  final PlantDTO plant;
+  final double horizontalSlide;
+  final AppHttpClient http;
+  late final String _imageUrl;
+
+  ParallaxPlantCard({
+    super.key,
+    required this.plant,
+    required this.horizontalSlide,
+    required this.http,
+  }) {
+    _imageUrl = "image/content/${plant.avatarImageId}";
+  }
+
+  @override
+  State<ParallaxPlantCard> createState() => _ParallaxPlantCard();
+}
+
+class _ParallaxPlantCard extends State<ParallaxPlantCard> {
+  String? _url;
+
+  @override
+  void initState() {
+    super.initState();
+    loadImageV1(widget._imageUrl, widget.http.key!);
+  }
+
+  void loadImageV1(String url, String key) async {
+    final response = await widget.http.get(url);
+    final blob = response.bodyBytes;
+    setState(() {
+      _url = 'data:image/jpg;base64,${base64Encode(blob)}';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: _url != null
+                  ? DecorationImage(
+                      image: CachedNetworkImageProvider(_url!),
+                      alignment: Alignment(
+                          widget.horizontalSlide, 0), // this does the parallax
+                      fit: BoxFit.cover,
+                    )
+                  : null,
+            ),
+            //alignment: Alignment(widget.horizontalSlide, 0),
+          ),
+        ),
+        Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.plant.info.personalName,
+                  softWrap: false,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  widget.plant.species!,
+                  softWrap: false,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(color: Colors.grey),
+                ),
+              ],
+            ))
+      ],
+    );
+  }
+}
