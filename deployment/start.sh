9c9ae6961c0cd618af947a9754591816e3d1bc4f
MDeLuise <66636702+MDeLuise@users.noreply.github.com>
2024-08-22 08:56:13 +0200

    wip

--- deployment/start.sh
+++ deployment/start.sh
@@ -3,16 +3,18 @@
 #######################
 #     Wait for DB     #
 #######################
-# Check if DEV is not set or is set to "false"
-if [ -z "$DEV" ] || [ "$DEV" = "false" ]; then
+if [ "$SQLITE" = "true" ]; then
+  export SPRING_PROFILES_ACTIVE=sqlite
+elif [ "$DEV" = "true" ]; then
+  export SPRING_PROFILES_ACTIVE=dev
+else
   /opt/app/wait-for-it.sh $MYSQL_HOST:$MYSQL_PORT -t 120 --;
   if [ $? -ne 0 ]; then
-      echo "DB (service name: $MYSQL_HOST, port: $MYSQL_PORT) not available, exiting.";
-      exit 1;
+    echo "DB (service name: $MYSQL_HOST, port: $MYSQL_PORT) not available, exiting.";
+    exit 1;
   fi
-else
-    export SPRING_PROFILES_ACTIVE=dev
 fi
+
 
 #######################
 #     Run backend     #
