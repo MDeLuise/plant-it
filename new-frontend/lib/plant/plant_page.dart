000a22429f080facfcf52bcea8c514e1331b4611
MDeLuise <66636702+MDeLuise@users.noreply.github.com>
2025-01-24 15:30:29 +0100

    wip: plant page

--- new-frontend/lib/plant/plant_page.dart
+++ new-frontend/lib/plant/plant_page.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
 import 'package:flutter_lucide/flutter_lucide.dart';
 import 'package:plant_it/database/database.dart';
 import 'package:plant_it/environment.dart';
-import 'package:plant_it/species_and_plant_widget_generator/plant_activity_widget_generator.dart';
+import 'package:plant_it/species_and_plant_widget_generator/plant_event_widget_generator.dart';
+import 'package:plant_it/species_and_plant_widget_generator/plant_reminder_widget_generator.dart';
 import 'package:plant_it/species_and_plant_widget_generator/species_care_widget_generator.dart';
 
 class PlantPage extends StatefulWidget {
@@ -20,12 +22,20 @@ class PlantPage extends StatefulWidget {
 class _PlantPageState extends State<PlantPage> {
   DecorationImage? _avatar;
   List<SpeciesCareInfoWidget>? speciesCareInfoWidgets;
-  List<PlantActivityInfoWidget>? plantActivityInfoWidgets;
-  bool _speciesCareLoad = true;
+  List<PlantEventInfoWidget>? plantEventInfoWidgets;
+  List<PlantReminderInfoWidget>? plantReminderInfoWidgets;
+  Specy? _species;
 
   @override
   void initState() {
     super.initState();
+
+    widget.env.speciesRepository.get(widget.plant.id).then((s) {
+      setState(() {
+        _species = s;
+      });
+    });
+
     _setPlantCare();
     widget.env.imageRepository.get(widget.plant.avatar!).then((i) {
       final DecorationImage newAvatar = DecorationImage(
@@ -39,24 +49,25 @@ class _PlantPageState extends State<PlantPage> {
   }
 
   void _setPlantCare() {
-    setState(() {
-      _speciesCareLoad = true;
-    });
-
     widget.env.speciesCareRepository.get(widget.plant.species).then((c) {
       final List<SpeciesCareInfoWidget> newSpeciesCareInfoWidgets =
           SpeciesCareWidgetGenerator(c).getWidgets();
       setState(() {
         speciesCareInfoWidgets = newSpeciesCareInfoWidgets;
-        _speciesCareLoad = false;
       });
     });
 
-    PlantActivityWidgetGenerator(widget.env, widget.plant)
+    PlantEventWidgetGenerator(widget.env, widget.plant).getWidgets().then((r) {
+      setState(() {
+        plantEventInfoWidgets = r;
+      });
+    });
+
+    PlantReminderWidgetGenerator(widget.env, widget.plant)
         .getWidgets()
         .then((r) {
       setState(() {
-        plantActivityInfoWidgets = r;
+        plantReminderInfoWidgets = r;
       });
     });
   }
@@ -118,7 +129,7 @@ class _PlantPageState extends State<PlantPage> {
                   ),
                   const SizedBox(height: 8),
                   Text(
-                    "X is a plant of the species Z, genus Y and family X.",
+                    "${widget.plant.name} is a plant of the species ${_species!.species}, genus ${_species!.genus} and family ${_species!.family}.",
                     style: Theme.of(context)
                         .textTheme
                         .bodyMedium!
@@ -133,107 +144,39 @@ class _PlantPageState extends State<PlantPage> {
                         color: Theme.of(context).colorScheme.secondary),
                   ),
                   const SizedBox(height: 8),
-
-                  GridView.builder(
-                    padding: const EdgeInsets.all(0),
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 3,
-                      crossAxisSpacing: 8,
-                      mainAxisSpacing: 8,
-                    ),
-                    itemCount: speciesCareInfoWidgets?.length ?? 0,
-                    itemBuilder: (context, index) {
-                      return speciesCareInfoWidgets!.elementAt(index);
-                    },
-                  ),
+                  _DynamicGridWidget(speciesCareInfoWidgets, 4),
                   const SizedBox(height: 16),
 
-                  // Activity
+                  // Reminder
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(
+                        'Reminders',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineSmall!
+                            .copyWith(
+                                color: Theme.of(context).colorScheme.secondary),
+                      ),
+                      Text("Edit"),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  _DynamicGridWidget(plantReminderInfoWidgets, 6),
+                  const SizedBox(height: 16),
+
+                  // Events
                   Text(
-                    'Activity',
+                    'Events',
                     style: Theme.of(context).textTheme.headlineSmall!.copyWith(
                         color: Theme.of(context).colorScheme.secondary),
                   ),
                   const SizedBox(height: 8),
-                  GridView.builder(
-                    padding: const EdgeInsets.all(0),
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 3,
-                      crossAxisSpacing: 8,
-                      mainAxisSpacing: 8,
-                    ),
-                    itemCount: plantActivityInfoWidgets?.length ?? 0,
-                    itemBuilder: (context, index) {
-                      return plantActivityInfoWidgets!.elementAt(index);
-                    },
-                  ),
-
-                  // // Horizontal Scroll for Activity Cards
-                  // SingleChildScrollView(
-                  //   scrollDirection: Axis.horizontal,
-                  //   child: Row(
-                  //     children: List.generate(activities.length, (index) {
-                  //       var activity = activities[index];
-
-                  //       // Only show the activities based on the current page index
-                  //       if (index >= _pageIndex * 6 &&
-                  //           index < (_pageIndex + 1) * 6) {
-                  //         return Container(
-                  //           margin: const EdgeInsets.only(right: 16),
-                  //           decoration: BoxDecoration(
-                  //             color: Colors.white.withOpacity(0.1),
-                  //             borderRadius: BorderRadius.circular(10),
-                  //           ),
-                  //           padding: const EdgeInsets.all(8),
-                  //           width: MediaQuery.of(context).size.width *
-                  //               0.4, // Adjust the width
-                  //           child: Row(
-                  //             children: [
-                  //               const SizedBox(width: 10),
-                  //               Icon(
-                  //                 LucideIcons.hand_helping,
-                  //                 color: Colors.white,
-                  //                 size: 30,
-                  //               ),
-                  //               const SizedBox(width: 20),
-                  //               Column(
-                  //                 crossAxisAlignment: CrossAxisAlignment.start,
-                  //                 children: [
-                  //                   Text(
-                  //                     activity['event']!,
-                  //                     style: Theme.of(context)
-                  //                         .textTheme
-                  //                         .bodyLarge!
-                  //                         .copyWith(color: Colors.white),
-                  //                   ),
-                  //                   const SizedBox(height: 4),
-                  //                   Text(
-                  //                     activity['time']!,
-                  //                     style: Theme.of(context)
-                  //                         .textTheme
-                  //                         .bodyMedium!
-                  //                         .copyWith(color: Colors.white70),
-                  //                   ),
-                  //                 ],
-                  //               ),
-                  //             ],
-                  //           ),
-                  //         );
-                  //       }
-
-                  //       return Container();
-                  //     }),
-                  //   ),
-                  // ),
-                  // const SizedBox(height: 16),
+                  _DynamicGridWidget(plantEventInfoWidgets, 6),
+                  const SizedBox(height: 16),
                 ],
               ),
             ),
@@ -241,5 +184,81 @@ class _PlantPageState extends State<PlantPage> {
         ],
       ),
     );
+  }
+}
+
+class _DynamicGridWidget extends StatefulWidget {
+  final int maxNum;
+  final List<Widget>? plantEventInfoWidgets;
+
+  const _DynamicGridWidget(this.plantEventInfoWidgets, this.maxNum);
+
+  @override
+  State<_DynamicGridWidget> createState() => _DynamicGridWidgetState();
+}
+
+class _DynamicGridWidgetState extends State<_DynamicGridWidget> {
+  late int pages;
+
+  @override
+  void initState() {
+    super.initState();
+    pages = (widget.plantEventInfoWidgets!.length / widget.maxNum).ceil();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (pages == 1) {
+      return _buildSinglePage(widget.plantEventInfoWidgets);
+    }
+    return _buildMultiplePages();
+  }
+
+  Widget _buildSinglePage(List<Widget>? tiles) {
+    return GridView.builder(
+      padding: const EdgeInsets.all(0),
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+      ),
+      itemCount: tiles?.length ?? 0,
+      itemBuilder: (context, index) {
+        return tiles!.elementAt(index);
+      },
+    );
+  }
+
+  Widget _buildMultiplePages() {
+    final List<List<Widget>> pagesList =
+        _chunkWidgets(widget.plantEventInfoWidgets!, widget.maxNum);
+
+    final List<Widget> pageWidgets =
+        pagesList.map((chunk) => _buildSinglePage(chunk)).toList();
+
+    // Use the pageWidgets in FlutterCarousel
+    return FlutterCarousel(
+      options: FlutterCarouselOptions(
+        showIndicator: true,
+        enableInfiniteScroll: false,
+        viewportFraction: 1,
+        indicatorMargin: 0,
+        height: 230,
+      ),
+      items: pageWidgets,
+    );
+  }
+
+  List<List<Widget>> _chunkWidgets(List<Widget> widgets, int chunkSize) {
+    List<List<Widget>> chunks = [];
+    for (var i = 0; i < widgets.length; i += chunkSize) {
+      final chunk = widgets.sublist(
+          i, i + chunkSize > widgets.length ? widgets.length : i + chunkSize);
+      chunks.add(chunk);
+    }
+    return chunks;
   }
 }
---
+++ new-frontend/lib/species_and_plant_widget_generator/plant_reminder_widget_generator.dart
@@ -0,0 +1,99 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+import 'package:plant_it/common.dart';
+import 'package:plant_it/database/database.dart';
+import 'package:plant_it/environment.dart';
+import 'package:plant_it/icons.dart';
+import 'package:plant_it/reminder/reminder_occurrence.dart';
+import 'package:plant_it/reminder/reminder_occurrence_service.dart';
+
+class PlantReminderWidgetGenerator {
+  final Environment env;
+  final Plant plant;
+  final Map<int, EventType> eventTypes = {};
+
+  PlantReminderWidgetGenerator(this.env, this.plant) {
+    env.eventTypeRepository.getAll().then((rl) {
+      for (EventType eventType in rl) {
+        eventTypes[eventType.id] = eventType;
+      }
+    });
+  }
+
+  Future<List<PlantReminderInfoWidget>> getWidgets() async {
+    final ReminderOccurrenceService reminderOccurrenceService =
+        ReminderOccurrenceService(env);
+
+    final List<Reminder> plantReminders =
+        await env.reminderRepository.getFiltered([plant.id], null);
+    final List<ReminderOccurrence> plantRemindersOccurrences = [];
+    for (Reminder r in plantReminders) {
+      plantRemindersOccurrences.add(
+          (await reminderOccurrenceService.getNextOccurrencesOfReminder(r, 1))
+              .first);
+    }
+
+    return plantRemindersOccurrences
+        .map((ro) => PlantReminderInfoWidget(
+            eventTypes[ro.reminder.type]!.name,
+            timeDiffStr(ro.nextOccurrence),
+            appIcons[eventTypes[ro.reminder.type]!.icon]!))
+        .toList();
+  }
+}
+
+class PlantReminderInfoWidget extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String value;
+
+  const PlantReminderInfoWidget(this.title, this.value, this.icon, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DottedBorder(
+      color: const Color.fromARGB(90, 255, 255, 255),
+      strokeWidth: 1,
+      borderType: BorderType.RRect,
+      radius: const Radius.circular(10),
+      dashPattern: const [5],
+      padding: EdgeInsets.zero,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: const EdgeInsets.all(8),
+        child: Row(
+          children: [
+            const SizedBox(width: 10),
+            Icon(
+              icon,
+              color: Colors.white,
+              size: 30,
+            ),
+            const SizedBox(width: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge!
+                      .copyWith(color: Colors.white),
+                ),
+                Text(
+                  value,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
